5f563483d29149cd82d6ae784691423e012b9331
asigba <alexsigba@gmail.com>
2025-09-08 22:48:30 -0400

    added dropdown

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import os
 
 root = tk.Tk()
 
@@ -6,5 +7,9 @@ root.title("File Organizer")
 root.geometry("400x300")
 
 button = tk.Button(root, text="Button", command=print("hello"))
+
+dropdown = tk.OptionMenu(root, tk.StringVar(), "Option 1", "Option 2")
+
 button.pack()
+dropdown.pack()
 root.mainloop()
